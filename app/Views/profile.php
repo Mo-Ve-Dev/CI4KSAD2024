fe880ebc451fdcea06995539a5dd207c8c6e3911
Mo-Ve-Dev <yohanes.brahmantya12@gmail.com>
2024-03-25 16:56:16 +0700

    commit 1

---
+++ app/Views/profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Intro</title>
+</head>
+
+<body>
+  <div>
+    <h1>Harro My Name Is :</h1>
+    <h2>Yohanes, i am a Programmer 💀👍</h2>
+  </div>
+</body>
+
+</html>
